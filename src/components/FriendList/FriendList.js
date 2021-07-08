a8e8d29f31b7811f2952fa66e46dad09dbfc21c3
Iryna Konoplova <a.irina773@gmail.com>
2021-07-08 11:03:36 +0300

    Корректирует FriendListItem

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -8,25 +8,12 @@ import FriendListItem from './FriendListItem';
 // Стили
 import styles from './FriendList.module.css';
 
+
 const FriendList = ({ friends }) => (
   <ul className={styles.friendList}>
-    <FriendList friends={friends} />,
+    <FriendListItem friends={friends} />,
   </ul>
 );
-
-// const FriendList = ({ friends }) => (
-//     <ul className={styles.friendList}>
-//         {friends.map((friend) => (   
-//             <FriendListItem
-//                 url={friend.avatar}
-//                 name={friend.name}
-//                 isOnline={friend.isOnline}
-//                 idItem={friend.id}
-//             />    
-//         )
-//         )}
-//     </ul>
-// );
 
 FriendList.propTypes = {
     friends: PropTypes.array.isRequired
--- src/components/FriendList/FriendListItem.js
+++ src/components/FriendList/FriendListItem.js
@@ -5,33 +5,15 @@ import PropTypes from 'prop-types';
 // Стили
 import styles from './FriendListItem.module.css';
 
+
 const FriendListItem = ({ friends }) =>
-  friends.map(({ friend }) => (
-    <li key={friend.id} className={styles.item}>
-      <span className={friend.isOnline ? styles.statusOnline : styles.status} />
-      <img className={styles.avatar} src={friend.avatar} alt={friend.name} width="48" />
-      <p className={styles.name}>{friend.name}</p>
+  friends.map(({avatar, name, isOnline, id }) => (
+    <li key={id} className={styles.item}>
+      <span className={isOnline ? styles.statusOnline : styles.status} />
+      <img className={styles.avatar} src={avatar} alt={name} width="48" />
+      <p className={styles.name}>{name}</p>
     </li>
   )); 
-
-// const FriendListItem = ({url, name, isOnline}) => (
-//     <li className={styles.item} >
-//         <span className={isOnline ? styles.statusOnline : styles.status} />
-//         <img className={styles.avatar} src={url} alt={name} width="48" />
-//         <p className={styles.name}>{name}</p>
-//     </li>
-// );
-
-// FriendListItem.defaultProps = {
-//     friend.avatar: "https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg"
-// };
-
-// FriendListItem.propTypes = {
-//     friend.avatar: PropTypes.string,
-//     friend.name: PropTypes.string.isRequired,
-//     friend.isOnline: PropTypes.bool.isRequired,
-//     friend.id: PropTypes.number.isRequired
-// };
 
 FriendListItem.propTypes = {
   friends: PropTypes.arrayOf(
